45f7d2ab00c8de6c3035e9bd6ee53c10acf01bf9
Steven Parker <stevenbparker@yahoo.com>
2022-04-14 16:50:54 -0400

    Updating for working flashcards application

--- myenv/FlashCards/main.py
+++ myenv/FlashCards/main.py
@@ -6,32 +6,43 @@ db = client["myflashcards"]
 collection = db["set1"]
 
 app = Flask(__name__)
+i = 0
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", upper="Hit NEXT to start")
 
 
 @app.route("/edit", methods=['GET', 'POST'])
-def edit_cards():
+def edit():
+    return render_template("edit.html")
+
+@app.route("/submit_new", methods=['GET', 'POST'])
+def submit_new():
     add = collection.insert_one(request.form.to_dict())
     return render_template("edit.html")
 
 
 @app.route("/next", methods=['GET', 'POST'])
 def next():
-    n = 0
-    if request.method == "POST":
-        card = list(collection.find())
-        if n == len(card):
-            n = 0
-            return render_template("index.html", upper=card[n])
-        else:
-            n = n+1
-            return render_template("index.html", upper=card[n])
+    card = list(collection.find())
+    global i
+    if i < (len(card)-1):
+        i = i + 1
+    elif i >= (len(card)-1):
+        i=0
+    return render_template("index.html", upper=card[i]['upper_card'], lower=card[i]['lower_card'])
+
+@app.route("/previous", methods=['GET', 'POST'])
+def previous():
+    card = list(collection.find())
+    global i
+    if i <= 0:
+        i=(len(card)-1)
     else:
-        return render_template("index.html")
+        i = i-1
+    return render_template("index.html", upper=card[i]['upper_card'], lower=card[i]['lower_card'])
 
 
 if __name__ == '__main__':
---
+++ myenv/FlashCards/static/script.js
@@ -0,0 +1,6 @@
+
+
+function myFunction(){
+    var hide_text = document.getElementById("lower_text");
+    hide_text.style.visibility = "visible";
+}
